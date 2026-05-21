8b4881d918c4c8354dd1ab5f4febefef566dda70
AECIO MENDES BRITO <aeciobrito@gmail.com>
2026-05-20 22:07:56 -0300

    Criação de serviço de geração de token

--- 02/ControleEstoque/ControleEstoque.API/Program.cs
+++ 02/ControleEstoque/ControleEstoque.API/Program.cs
@@ -1,6 +1,9 @@
 using ControleEstoque.API.Data;
 using ControleEstoque.API.Services;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 using System.Text.Json.Serialization;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -16,11 +19,31 @@ builder.Services.AddScoped<IFornecedorService, FornecedorService>();
 builder.Services.AddScoped<IUsuarioService, UsuarioService>();
 builder.Services.AddScoped<IContaReceberService, ContaReceberService>();
 builder.Services.AddScoped<IPasswordService, PasswordService>();
+builder.Services.AddScoped<ITokenService, TokenService>();
 builder.Services.AddControllers().AddJsonOptions(options =>
 {
     // encerra o erro de refer�ncia c�clica de objetos para o JSON
     options.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles;
 });
+
+
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuer = true,
+            ValidateAudience = true,
+            ValidateLifetime = true,
+            ValidateIssuerSigningKey = true,
+            ValidIssuer = builder.Configuration["Jwt:Emissor"],
+            ValidAudience = builder.Configuration["Jwt:Audiencia"],
+            IssuerSigningKey = new SymmetricSecurityKey(
+                Encoding.UTF8.GetBytes(builder.Configuration["Jwt:ChaveSecreta"])) ??
+                throw new InvalidOperationException("Chave JWT não configurada")
+        };
+    });
+
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
---
+++ 02/ControleEstoque/ControleEstoque.API/Services/TokenService.cs
@@ -0,0 +1,45 @@
+﻿using ControleEstoque.API.Models;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace ControleEstoque.API.Services
+{
+    public class TokenService : ITokenService
+    {
+        private readonly IConfiguration _configuration;
+
+        public TokenService(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public string GerarToken(Usuario usuario)
+        {
+            var handler = new JwtSecurityTokenHandler();
+            var chave = Encoding.ASCII.GetBytes(_configuration["Jwt:ChaveSecreta"]!);
+
+            // Claims
+            var claims = new ClaimsIdentity(new[]
+            {
+                new Claim(ClaimTypes.NameIdentifier, usuario.Id.ToString()),
+                new Claim(ClaimTypes.Name, usuario.Nome),
+                new Claim(ClaimTypes.Email, usuario.Email),
+                new Claim(ClaimTypes.Role, usuario.Perfil.ToString())
+            });
+
+            var descriptor = new SecurityTokenDescriptor
+            {
+                Subject = claims,
+                Expires = DateTime.UtcNow.AddHours(8),
+                SigningCredentials = new SigningCredentials(
+                    new SymmetricSecurityKey(chave),
+                    SecurityAlgorithms.HmacSha256Signature)
+            };
+
+            var token = handler.CreateToken(descriptor);
+            return handler.WriteToken(token);
+        }
+    }
+}
